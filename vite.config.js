a5b87ef65e766fceff42c49b9500b0a661f271a1
mrshivam77 <mrshivamraj77@gmail.com>
2025-03-12 20:17:55 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),   
-     tailwindcss(),
-],base: '/',
-})
-  
+  plugins: [react(), tailwindcss()],
+  base: '/',
+  server: {
+    port: 5173, // Optional: for local dev
+  },
+  build: {
+    outDir: 'dist', // Where your build files go
+  }
+});
