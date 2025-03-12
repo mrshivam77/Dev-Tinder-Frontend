9f9d4a8a2e9bf1cf3bffa31accdeecd98f3b106a
mrshivam77 <mrshivamraj77@gmail.com>
2025-03-12 20:02:45 +0530

    latest commits

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,1 @@
-export const BASEURL = 
-  window.location.hostname === "dev-tinder-frontend.onrender.com"
-    ? "https://dev-tinder-backend-4sfg.onrender.com" // Production backend
-    : "http://localhost:5000"; // Local backend (for development)
+export const  BASEURL="https://dev-tinder-backend-4sfg.onrender.com"
